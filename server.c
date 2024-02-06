af3c6b3423513b815ab982f03b49585fc8dc0c4b
Valerian <libertvalerian@gmail.com>
2024-02-06 12:57:37 +0100

    m

--- server.c
+++ server.c
@@ -6,37 +6,58 @@
 /*   By: vlibert <vlibert@students.42.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:59:46 by vlibert           #+#    #+#             */
-/*   Updated: 2023/12/12 11:55:27 by vlibert          ###   ########.fr       */
+/*   Updated: 2024/02/06 12:56:43 by vlibert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
-#include <unistd.h>
 
-void	sig_handler(int sig, siginfo_t *info, void *context)
+void	sig_handler(int sig)
 {
-	printf("Youpi");
-	//if(sig == SIGUSER1)
-		
-	//if (sig == SIGUSER2)
-	
-}
+	char c;
+	int i;
 
-int main()
-{
-	struct	sigaction sa;
-	pid_t	pid;
-	
-	sa.sa_sigaction = sig_handler;
-	sa.sa_flags = SA_SIGINFO;
-	pid = getpid();
-	printf("Here is my PID : %i", pid);
-	while(1)
+	i = 0;
+	while (i < 8)
 	{
-		sigaction(SIGUSR1, &sa, NULL);
-		sigaction(SIGUSR1, &sa, NULL);
-		sleep(100);
+		if(sig == SIGUSR1)
+			c << 1;
+		else
+			c |= (c << bit);
+			
+		
 	}
 }
+/**/
+int main(void)
+{
+    struct sigaction    sa, osa;
+
+    sa.sa_flags = 0;
+    sa.sa_handler = sig_handler;
+    sigaction(SIGUSR1, &sa, &osa);
+    sigaction(SIGUSR2, &sa, &osa);
+	printf("Here is my PID : %d\n", getpid());
+    while (1)
+        usleep(1);
+    return(0);
+}
+/*
+int main()
+{
+	struct	sigaction starfoula;
+	sigset_t	mask;
+	
+	printf("Here is my PID : %d", getpid());
+	starfoula.sa_flags = SA_SIGINFO;
+	starfoula.sa_sigaction = sig_handler;
+	
+	while(1)
+	{
+		//usleep(1000000000);
+	}
+
+}*/
